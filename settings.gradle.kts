1241bdc18828c230ea4e15f69e01d4bf716449c7
mckasapbasi <mckasap@gmail.com>
2024-03-06 12:08:29 +0300

    test

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MYO2024Hafta03_03"
+include(":app")
+ 
